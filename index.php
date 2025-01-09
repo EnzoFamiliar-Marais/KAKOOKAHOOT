cb2aeef38021d1b3067002fd8e4ff98a672c96e5
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2025-01-09 09:58:00 +0100

    fe

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
 <?php
 
 
-class Autoloader{
-    static function register(){
-        spl_autoload_register(array(__CLASS__, 'autoload'));
-    }
-
-    static function autoload($fqcn) {
-        $path = str_replace('\\', '/', $fqcn);
-        require 'Classes/' . $path . '.php';
-    }
-}
 
 
 $question_total = 0;
