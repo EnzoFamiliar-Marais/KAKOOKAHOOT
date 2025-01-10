26b12fb57b6a808b555b0252cfbf6deac9a5e5da
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2025-01-10 11:57:13 +0100

    reponses fonctionnel

--- Action/Reponses.php
+++ Action/Reponses.php
@@ -1,33 +1,33 @@
 <?php
 function answer_text($q, $v) {
     global $question_correct, $score_total, $score_correct;
-    $score_total += $q["score"];
+    $score_total += $q->getScore();
     if (is_null($v)) return;
-    if ($q["answer"] == $v) {
+    if ($q->getAnswer() == $v) {
         $question_correct += 1;
-        $score_correct += $q["score"];
+        $score_correct += $q->getScore();
     }
 }
 
 function answer_radio($q, $v) {
     global $question_correct, $score_total, $score_correct;
-    $score_total += $q["score"];
+    $score_total += $q->getScore();
     if (is_null($v)) return;
-    if ($q["answer"] == $v) {
+    if ($q->getAnswer() == $v) {
         $question_correct += 1;
-        $score_correct += $q["score"];
+        $score_correct += $q->getScore();
     }
 }
 
 function answer_checkbox($q, $v) {
     global $question_correct, $score_total, $score_correct;
-    $score_total += $q["score"];
+    $score_total += $q->getScore();
     if (is_null($v)) return;
-    $diff1 = array_diff($q["answer"], $v);
-    $diff2 = array_diff($v, $q["answer"]);
+    $diff1 = array_diff($q->getAnswer(), $v);
+    $diff2 = array_diff($v, $q->getAnswer());
     if (count($diff1) == 0 && count($diff2) == 0) {
         $question_correct += 1;
-        $score_correct += $q["score"];
+        $score_correct += $q->getScore();
     }
 }
 
@@ -43,10 +43,22 @@ function verifier_reponses($questions, $post_data) {
 
     foreach ($questions as $q) {
         $question_total += 1;
-        $answer_handlers[$q["type"]]($q, $post_data[$q["name"]] ?? NULL);
+        $answer_handlers[$q->getType()]($q, $post_data[$q->getName()] ?? NULL);
     }
 
+    reponses_questions($questions, $post_data);
     echo "Réponses correctes: $question_correct/$question_total<br>";
     echo "Votre score: $score_correct/$score_total<br>";
 }
+
+function reponses_questions($questions, $post_data) {
+    global $answer_handlers;
+    foreach ($questions as $q) {
+        $answer_handlers[$q->getType()]($q, $post_data[$q->getName()] ?? NULL);
+        echo "Question: " . $q->getText() . "<br>";
+        echo "Votre réponse: " . ($post_data[$q->getName()] ?? "Aucune réponse") . "<br>";
+        echo "Réponse correcte: " . implode(", ", (array)$q->getAnswer()) . "<br><br>";
+    }
+}
+
 ?>
