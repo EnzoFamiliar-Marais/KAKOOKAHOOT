026c8f02be071c588b3646a5c20f237f55950cd8
Deflandre David <o22302048@or-iut-i206-f10.iut45.univ-orleans.fr>
2025-01-10 12:02:16 +0100

    Ajout du fichier quiz

---
+++ Action/Quiz.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<title>Quiz</title>
+</head>
+<body>
+<?php
+
+
+require_once '../Classes/autoloader.php';
+require_once '../Classes/Form/QuestionProvider.php';
+require_once '../Action/Questions.php';
+require_once '../Action/Reponses.php';
+
+$question_total = 0;
+$question_correct = 0;
+$score_total = 0;
+$score_correct = 0;
+
+$provider = new Classes\Form\QuestionProvider('../Data/questions.JSON');
+$questions = $provider->getQuestions();
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    afficher_questions($questions);
+} else {
+    verifier_reponses($questions, $_POST);
+}
+
+
+$action = $_REQUEST["action"] ?? false;
+switch ($action) {
+    case "submit":
+        break;
+    default:
+        break;
+}
+
+?>
+</body>
+</html>
